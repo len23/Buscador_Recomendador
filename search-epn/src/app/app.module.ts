4f27a4602f71c9c7bec20140595594f098357686
Unknown <lenin.montalvo.23@hotmail.com>
2019-01-13 18:26:50 -0500

    Rutas y componentes

--- search-epn/src/app/app.module.ts
+++ search-epn/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+import { BibliotecaComponent } from './biblioteca/biblioteca.component';
+import { MisArticulosComponent } from './mis-articulos/mis-articulos.component';
+import { RecomendacionesComponent } from './recomendaciones/recomendaciones.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    BusquedaComponent,
+    BibliotecaComponent,
+    MisArticulosComponent,
+    RecomendacionesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
