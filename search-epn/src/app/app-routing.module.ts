6f26085f175bbfb203116413877cb631231c7eb9
Unknown <lenin.montalvo.23@hotmail.com>
2019-01-13 18:38:47 -0500

    Rutas

--- search-epn/src/app/app-routing.module.ts
+++ search-epn/src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+import { BibliotecaComponent } from './biblioteca/biblioteca.component';
+import { MisArticulosComponent } from './mis-articulos/mis-articulos.component';
+import { RecomendacionesComponent } from './recomendaciones/recomendaciones.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'busqueda', component:BusquedaComponent},
+  { path: 'biblioteca', component:BibliotecaComponent},
+  { path: 'crear_mis_articulos', component:MisArticulosComponent},
+  { path: 'recomendaciones', component:RecomendacionesComponent},
 ];
 
 @NgModule({
-  imports: [
-RouterModule.forRoot(routes) ],
-exports: [ RouterModule ],
-declarations: []
+  imports: [RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
